f1a0c0fe53b11f2a514033a6350e0aac91df83cb
ramakrishna3183 <112241148+ramakrishna3183@users.noreply.github.com>
2023-08-30 08:16:08 +0530

    Create 14.Write a program   to find the length of a string..cpp

---
+++ 14.Write a program   to find the length of a string..cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int  main()
+{
+	char a[20];
+	int i=0,count=0;
+	printf("Enter a string:");
+	scanf("%s",&a);
+	while(a[i]!='\0'){
+		count+=1;
+		i++;
+}
+	printf("The length of string is:%d",(count));
+}
